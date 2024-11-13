b4a31ceffee04ecb0b6f4465545c988e11774271
Reo Nagahama <92760959+ReoN-00@users.noreply.github.com>
2024-11-13 12:52:36 -1000

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import './globals.css';
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center vh-100">
+      <Row id="about" className="about-row align-middle text-center vh-100">
         <Col xs={12} className="d-flex flex-column justify-content-center align-items-center">
           <h1>Study Buddy</h1>
           <p>&quot;Your study success starts with a buddy&quot;</p>
           <Button>Get Started</Button>
         </Col>
       </Row>
-      <Row className="tutorial-row vh-100">
+      <Row id="tutorial" className="tutorial-row vh-100">
         <h1>What is Study Buddy?</h1>
       </Row>
     </Container>
