0b52c8d0033997aac2a252bd572ba87ffaa28f66
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-11-20 04:22:46 -1000

    fixed homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,19 @@ import './globals.css';
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
-      <Row id="about" className="about-row align-middle text-center vh-100">
+      <Row id="home" className="about-row align-middle text-center vh-100">
         <Col xs={12} className="d-flex flex-column justify-content-center align-items-center">
-          <h1 className="title">Study</h1>
-          <h1 className="title">Buddy</h1>
-          <p>&quot;Your study success starts with a buddy&quot;</p>
+          <h1 className="title">
+            <strong>Study</strong>
+          </h1>
+          <h1 className="title">
+            <strong>Buddy</strong>
+          </h1>
+          <h2>&quot;Your study success starts with a buddy&quot;</h2>
           <Button className="get-started">Get Started</Button>
         </Col>
       </Row>
-      <Row id="tutorial" className="tutorial-row vh-100">
+      <Row id="about" className="tutorial-row vh-100">
         <h1 className="mt-5">What is Study Buddy?</h1>
         <Col className="col-centered">
           <div className="container_temp">
@@ -24,9 +28,18 @@ const Home = () => (
           <div className="container">
             <div className="overlay" />
             <p className="text">
-              Study buddy is an application to find a buddy to study with.
-              With study buddy, you can find and schedule study sessions.
+              Study buddy is an application to find a buddy to study with. With study buddy, you can find and schedule
+              study sessions.
             </p>
+          </div>
+        </Col>
+      </Row>
+      <Row id="tutorial" className=" vh-100">
+        <h1 className="mt-5">Tutorial</h1>
+        <Col className="col-centered">
+          <div className="container">
+            <div className="overlay" />
+            <p className="text">This is the video demo</p>
           </div>
         </Col>
       </Row>
