73d08bb0ecb4b134d49b14ba3a8c0721e1a5738a
Wilson Huynh <132548102+wilsonh22@users.noreply.github.com>
2024-11-29 00:40:50 -1000

    CollegeROle section

--- src/app/myProfile/page.tsx
+++ src/app/myProfile/page.tsx
@@ -1,6 +1,6 @@
 import { Profile } from '@prisma/client';
 import { getServerSession } from 'next-auth';
-import { Button } from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 import '../../styles/myProfile.style.css';
 import { prisma } from '@/lib/prisma';
 import authOptions from '@/lib/auth';
@@ -38,13 +38,18 @@ const myProfile = async () => {
                   {profile.major}
                 </h5>
                 <h5>
-                  <strong>Bio: </strong>
-                  {profile.bio}
+                  <strong>College Role:</strong>
+                  {profile.collegeRole}
                 </h5>
                 <h5>
                   <strong>Socials: </strong>
                   {profile.social}
                 </h5>
+                <h5>
+                  <strong>Bio: </strong>
+                  {profile.bio}
+                </h5>
+
                 {/* add profile.major here within h5 tag and other datafields */}
               </div>
             ))}
