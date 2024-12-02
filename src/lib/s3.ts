fd5ef542e370d66515d42edf08f9cd37bf3acafe
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-12-02 09:58:11 -1000

    fixed s3.ts keys

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,10 +1,10 @@
 import AWS from 'aws-sdk';
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  region: process.env.NEXT_PUBLIC_AWS_REGION,
-  params: { Bucket: process.env.NEXT_PUBLIC_S3_BUCKET },
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  params: { Bucket: process.env.S3_BUCKET },
 });
 
 export default s3;
