ba04daa308ffdc48985830bd29ea9f71a018889a
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-12-03 10:13:46 -1000

    trying arn role

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,10 +1,22 @@
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+//   region: process.env.NEXT_PUBLIC_AWS_REGION,
+//   params: { Bucket: process.env.NEXT_PUBLIC_S3_BUCKET },
+// });
+
+// export default s3;
+
+import { S3Client } from '@aws-sdk/client-s3';
+import { awsCredentialsProvider } from '@vercel/functions/oidc';
+
+const s3 = new S3Client({
   region: process.env.NEXT_PUBLIC_AWS_REGION,
-  params: { Bucket: process.env.NEXT_PUBLIC_S3_BUCKET },
+  credentials: awsCredentialsProvider({
+    roleArn: process.env.NEXT_PUBLIC_AWS_ROLE_ARN!,
+  }),
 });
 
 export default s3;
