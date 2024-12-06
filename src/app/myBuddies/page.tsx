6a278e7cd0e10ff121122edd04728c48a7914813
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-12-05 22:21:17 -1000

    created myBuddies page

---
+++ src/app/myBuddies/page.tsx
@@ -0,0 +1,56 @@
+// 'use client';
+
+// import { useState } from 'react';
+import authOptions from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/lib/prisma';
+import { Buddy } from '@prisma/client';
+// import SearchBuddies from '../../components/SearchBuddies';
+import BuddyCard from '../../components/BuddyCard';
+import '../../styles/buddies.style.css';
+
+type ExtendedBuddy = Buddy & {
+  userDupe: {
+    id: number;
+    profile?: {
+      firstName: string;
+      lastName: string;
+    };
+  };
+};
+
+const myBuddies = async () => {
+  // const [search, setSearch] = useState('');
+
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.email) {
+    return <div>Session not found</div>;
+  }
+  const userSession = session as unknown as { user: { email: string; id: string; randomKey: string } };
+  const currentUser = parseInt(userSession.user.id, 10);
+
+  // Fetch buddies on the server
+
+  const buddyList: ExtendedBuddy[] = (await prisma.buddy.findMany({
+    include: {
+      userDupe: {
+        include: {
+          profile: true,
+        },
+      },
+    },
+  })) as ExtendedBuddy[];
+
+  return (
+    <div className="buddies">
+      <h1 className="buddiesPageTitle">
+        <strong>Buddies</strong>
+      </h1>
+      {/* <SearchBuddies search={search} setSearch={setSearch} /> */}
+      <div className="buddiesListDiv">
+        <BuddyCard buddyList={buddyList} currentUser={currentUser} />
+      </div>
+    </div>
+  );
+};
+export default myBuddies;
