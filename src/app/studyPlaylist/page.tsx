7486576af916d3b7e83cce7627aa4b560859ddfb
Wilson Huynh <132548102+wilsonh22@users.noreply.github.com>
2024-12-09 20:57:51 -1000

    eslint

--- src/app/studyPlaylist/page.tsx
+++ src/app/studyPlaylist/page.tsx
@@ -4,26 +4,9 @@
 import authOptions from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { prisma } from '@/lib/prisma';
-import { Buddy } from '@prisma/client';
 // import SearchBuddies from '../../components/SearchBuddies';
 import { Card } from 'react-bootstrap';
-import BuddyCard from '../../components/BuddyCard';
 import '../../styles/studyPlaylist.style.css';
-
-type ExtendedBuddy = Buddy & {
-  userDupe: {
-    id: number;
-    profile?: {
-      firstName: string;
-      lastName: string;
-      bio: string;
-      major: string;
-      collegeRole: string;
-      social: string;
-      profilePicUrl: string;
-    };
-  };
-};
 
 const studyPlaylist = async () => {
   // const [search, setSearch] = useState('');
@@ -32,21 +15,6 @@ const studyPlaylist = async () => {
   if (!session || !session.user || !session.user.email) {
     return <div>Session not found</div>;
   }
-  const userSession = session as unknown as { user: { email: string; id: string; randomKey: string } };
-  const currentUser = parseInt(userSession.user.id, 10);
-
-  // Fetch buddies on the server
-
-  const buddyList: ExtendedBuddy[] = (await prisma.buddy.findMany({
-    include: {
-      userDupe: {
-        include: {
-          profile: true,
-        },
-      },
-    },
-  })) as ExtendedBuddy[];
-
   return (
     <div className="playlist">
       {/* Page Title */}
